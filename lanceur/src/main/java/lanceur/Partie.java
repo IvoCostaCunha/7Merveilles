4931f6c0c1f00a07ae9563beaa42a1776361c598
Ivo Costa Cunha <ivocostacunha@hotmail.fr>
2019-02-15 14:36:40 +0100

    Ajout au lanceur de 3 autres cliens

--- lanceur/src/main/java/lanceur/Partie.java
+++ lanceur/src/main/java/lanceur/Partie.java
@@ -21,8 +21,34 @@ public class Partie {
             }
         });
 
+        Thread client2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Client.main(null);
+            }
+        });
+        Thread client3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Client.main(null);
+            }
+        });
+
+        Thread client4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Client.main(null);
+            }
+        });
+
+        /*---------- Lancement du serveur ----------*/
         serveur.start();
+
+        /*---------- Lancement des clients ----------*/
         client.start();
+        client2.start();
+        client3.start();
+        client4.start();
 
     }
 }
