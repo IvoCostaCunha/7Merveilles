724f41fe7fa0fe921125e4df251599a842b4749c
Ivo Costa Cunha <ivocostacunha@hotmail.fr>
2019-02-26 21:58:05 +0100

    ajout fonctions de initialisation d'elements de jeu principaux

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,20 +1,25 @@
 package client;
 
-import commun.Joueur;
-import commun.Plateau;
-import commun.Merveille;
-import commun.Carte;
+import commun.*;
+
+// Imports SocketIO
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+
+// Imports JSON
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+// Autres imports
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+
+// Faut arreter les clients si le serveur meurt, kill les processus ...
+
 
 public class Client {
 
@@ -29,21 +34,19 @@ public class Client {
     // Objet de synchro
     final Object attenteDéconnexion = new Object();
 
-    public Client(String urlServeur) {
+    /**
+     * Construit un objet Client
+     * @param ipServeur IP de l'adresse a qui connecter l'objet Client
+     */
+    public Client(String ipServeur) {
     	// On fait en sorte que des joueurs soient crées
-    	
-    	
         try {
-            connexion = IO.socket(urlServeur);
-
+            connexion = IO.socket(ipServeur);
             System.out.println("Vous avez rejoint la partie");
 
             connexion.on("connect", new Emitter.Listener() {
                 @Override
-                public void call(Object... objects) {
-
-                    connexion.emit("rejoindrePartie");
-                }
+                public void call(Object... objects) { connexion.emit("rejoindrePartie"); }
             });
 
             connexion.on("disconnect", new Emitter.Listener() {
@@ -52,13 +55,9 @@ public class Client {
                     System.out.println("Deconnexion");
                     connexion.disconnect();
                     connexion.close();
-
-                    synchronized (attenteDéconnexion) {
-                        attenteDéconnexion.notify();
-                    }
+                    synchronized (attenteDéconnexion) { attenteDéconnexion.notify(); }
                 }
             });
-
 
             // on recoit une question
            connexion.on("nbJoueurs", new Emitter.Listener() {
@@ -68,29 +67,9 @@ public class Client {
                 }
             });
 
-            connexion.on("lancerPartie", new Emitter.Listener() {
+            connexion.on("msgDebutPartie", new Emitter.Listener() {
                 @Override
-                public void call(Object... objects) {
-                    System.out.println("QUE LE JEU COMMENCE !!!");
-                    /*for (int i=0;i<4;i++)
-                	{
-                		listeJoueursClient.add(new Joueur(i+1));
-                		System.out.println("Joueur"+i+"prêt");
-                	}
-                	// Ces joueurs choisissent leurs cartes
-                	for (Joueur j : listeJoueursClient)
-                	{
-                		
-                		listePlateauxClient = j.choisirPlateau(listePlateauxClient);
-                		// On distribue 7 cartes pour chaque joueur
-                		for (int i = 0;i<7;i++)
-                		{
-                			listeCarteClient.add(j.choisirCarte());
-                			System.out.println("Distribution d'une carte");
-                		}
-                		
-                	}*/
-                }
+                public void call(Object... objects) { System.out.println("QUE LE JEU COMMENCE !!!"); }
             });
 
 
@@ -105,11 +84,10 @@ public class Client {
         // on se connecte
         connexion.connect();
 
-        //System.out.println("en attente de déconnexion");
+        // System.out.println("en attente de déconnexion");
         synchronized (attenteDéconnexion) {
-            try {
-                attenteDéconnexion.wait();
-            } catch (InterruptedException e) {
+            try { attenteDéconnexion.wait(); }
+            catch (InterruptedException e) {
                 e.printStackTrace();
                 System.err.println("erreur dans l'attente");
             }
@@ -117,19 +95,13 @@ public class Client {
     }
 
     public static final void main(String []args) {
-        try {
-            System.setOut(new PrintStream(System.out, true, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        try { System.setOut(new PrintStream(System.out, true, "UTF-8")); }
+        catch (UnsupportedEncodingException e) { e.printStackTrace(); }
 
         Client client = new Client("http://127.0.0.1:557");
         client.seConnecter();
 
-
-
         System.out.println("fin du main pour le client");
-
     }
 
 }
--- serveur/src/main/java/serveur/Serveur.java
+++ serveur/src/main/java/serveur/Serveur.java
@@ -11,19 +11,32 @@ import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
+import commun.*;
+
 /**
  * attend une connexion, on envoie une question puis on attend une réponse, jusqu'à la découverte de la bonne réponse
  * le client s'identifie (som, niveau)
  */
 public class Serveur {
 
-    SocketIOServer serveur;
-    final Object attenteConnexion = new Object();
-    int nbJoueurs = 0;
-    ArrayList<SocketIOClient> listeClients = new ArrayList<SocketIOClient>();
+    private SocketIOServer serveur;
+    private final Object attenteConnexion = new Object();
+
+    /* ---------- Infos clients connectés ---------- */
+    private int nbJoueurs = 0;
+    private ArrayList<SocketIOClient> listeClients = new ArrayList<SocketIOClient>();
+
+    /* ---------- Elements de jeu initiaux ---------- */
+    private ArrayList<ArrayList<Carte>> decksCirculants = new ArrayList<ArrayList<Carte>>();
+    private ArrayList<Carte> deckCirculant1 = new ArrayList<Carte>(7);
+    private ArrayList<Carte> deckCirculant2 = new ArrayList<Carte>(7);
+    private ArrayList<Carte> deckCirculant3 = new ArrayList<Carte>(7);
+    private ArrayList<Carte> deckCirculant4 = new ArrayList<Carte>(7);
 
 
-    public void ajouterJoueur() {
+
+
+    public void incrementerNbJoueurs() {
     	nbJoueurs++;
     }
 
@@ -31,12 +44,15 @@ public class Serveur {
     	return nbJoueurs;
     }
 
+    /**
+     * Constructeur de la classe serveur
+     * @param config objet de configuration du serveur
+     */
     public Serveur(Configuration config) {
-        // creation du serveur
+        // Création du serveur
         serveur = new SocketIOServer(config);
 
-        // Objet de synchro
-
+        // Objet de synchronisation
         System.out.println("préparation du listener");
 
         // on accepte une connexion
@@ -44,68 +60,30 @@ public class Serveur {
             public void onConnect(SocketIOClient socketIOClient) {
             	listeClients.add(socketIOClient);
                 System.out.println("\nUne connexion effectuee");
-                nbJoueurs++;
+                incrementerNbJoueurs();
 	        	donnerNbJoueurs(socketIOClient, nbJoueurs);
-	        	if(nbJoueurs == 4) {
-	        		lancerPartie(socketIOClient);
-	        	}
-
+                System.out.println("connexion du client numero " + nbJoueurs);
+	        	if(nbJoueurs == 4) { lancerPartie(); }
             }
         });
 
 
-        serveur.addEventListener("rejoindrePartie", Object.class, new DataListener<Object>() {
+        /*serveur.addEventListener("rejoindrePartie", Object.class, new DataListener<Object>() {
 	    @Override
 	    public void onData(SocketIOClient socketIOClient, Object objVide, AckRequest ackRequest) throws Exception {
-	       
 	        System.out.println("connexion du client numero " + nbJoueurs);
-	   		}
-		});
+	        }
+        });*/
 
 
 
     }
 
-
-    private void démarrer() {
-
-        serveur.start();
-
-        System.out.println("en attente de connexion");
-        synchronized (attenteConnexion) {
-            try {
-                attenteConnexion.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                System.err.println("erreur dans l'attente");
-            }
-        }
-
-        System.out.println("Une connexion est arrivée, on arrête");
-        serveur.stop();
-
-    }
-
-
-    private void donnerNbJoueurs(SocketIOClient socketIOClient, int nbJoueurs) {
-        socketIOClient.sendEvent("nbJoueurs", nbJoueurs);
-    }
-
-    private void lancerPartie(SocketIOClient socketIOClient) {
-        for(SocketIOClient client: listeClients) {
-        	client.sendEvent("lancerPartie");
-        	//Envoyer du JSON (cartes)
-        }
-    }
-
-
+    /* ----------- méthode main ----------- */
 
     public static final void main(String []args) {
-        try {
-            System.setOut(new PrintStream(System.out, true, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        try { System.setOut(new PrintStream(System.out, true, "UTF-8")); }
+        catch (UnsupportedEncodingException e) { e.printStackTrace(); }
 
         Configuration config = new Configuration();
         config.setHostname("127.0.0.1");
@@ -118,6 +96,77 @@ public class Serveur {
 
         System.out.println("fin du main");
 
+    }
+
+
+
+    private void démarrer() {
+
+        serveur.start();
+
+        System.out.println("en attente de connexion");
+        synchronized (attenteConnexion) {
+            try { attenteConnexion.wait(); }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+                System.err.println("erreur dans l'attente");
+            }
+        }
+
+        System.out.println("Une connexion est arrivée, on arrête");
+        serveur.stop();
+
+    }
+
+
+    /**
+     * Méthode qui envoie au client le nb de joueurs
+     * @param socketIOClient Objet socketIOClient a qui envoyer
+     * @param nbJoueurs le nb de joueurs
+     */
+    private void donnerNbJoueurs(SocketIOClient socketIOClient, int nbJoueurs) {
+        socketIOClient.sendEvent("nbJoueurs", nbJoueurs);
+    }
+
+    /**
+     * Méthode qui lance la partie
+     */
+    private void lancerPartie() {
+        for(SocketIOClient client: listeClients) {
+        	client.sendEvent("msgDebutPartie");
+        	//Envoyer du JSON (cartes)
+        }
+    }
+
+    /**
+     * Méthode qui instancie TOUS éléments de jeu initiaux
+     */
+    private void initialisationElemJeu(){
+        initialisationDecksCirculants();
+    }
+
+    /**
+     * Méthode qui instancie les cecks circulants
+     */
+    private void initialisationDecksCirculants(){
+        for(ArrayList<Carte> deck : decksCirculants){
+            for(int i=0;i<7; i++){
+                deck.add(new Carte("Carte"+(i+1),(int)Math.random()*20));
+            }
+        }
+    }
+
+    /**
+     * Méthode qui initialise les plateaux
+     */
+    private void initialiserPleateaux(){
+        for(int i=0;i<7;i++){
+            // Generer les merveilles de manière aléatoire NB + POINTS
+            ArrayList<Merveille>
+
+            String nomPlateau = "plateau"+(i+1);
+            Plateau plateau+(i+1) = new Plateau()
+        }
     }
 
 
