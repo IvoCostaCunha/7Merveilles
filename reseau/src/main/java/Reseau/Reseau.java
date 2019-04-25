9325698dafcb820abda5bb733a4a7b344f9d4881
vincent Bourdon <vincent.bourdon41@gmail.com>
2019-04-25 21:02:03 +0200

    tentative de création de la classe Réseau - echec. Doit refaire les tests car différents types de cartes mais bugs (null) et touj ours même points pour les joueurs

---
+++ reseau/src/main/java/Reseau/Reseau.java
@@ -0,0 +1,34 @@
+package lanceur;
+
+import client.Client;
+import com.corundumstudio.socketio.Configuration;
+import serveur.Serveur;
+
+public class Partie {
+
+    public final static void main(String [] args) {
+
+        Configuration config = new Configuration();
+        config.setHostname("127.0.0.1");
+        config.setPort(557);
+
+        Serveur s = new Serveur(config);
+        s.démarrer();
+
+        /* Instanciations des clients en cas normal */
+        Client [] c = new Client[2];
+        for(int i = 0; i < c.length; i++)  {
+            c[i] = new Client("http://127.0.0.1:557");
+            c[i].seConnecter();
+        }
+
+        /* Pour les tests */
+        /*Client c= new Client("http://127.0.0.1:557");
+        c.seConnecter();*/
+
+        /* Instanciation d'un client pour simplifier les tests */
+        //Client unClient = new Client("http://127.0.0.1:557");
+        //unClient.seConnecter();
+
+    }
+}
