2a539a6c41a6bfccbfef7f127646eec9710f914c
Ivo Costa Cunha <ivo.costacunha@icloud.com>
2024-10-18 02:30:13 +0100

    changed port of server

--- lanceur/src/main/java/lanceur/Partie.java
+++ lanceur/src/main/java/lanceur/Partie.java
@@ -10,7 +10,7 @@ public class Partie {
 
         Configuration config = new Configuration();
         config.setHostname("127.0.0.1");
-        config.setPort(557);
+        config.setPort(8001);
 
         Serveur s = new Serveur(config);
         s.démarrer();
@@ -18,7 +18,7 @@ public class Partie {
         /* Instanciations des clients en cas normal */
         Client [] c = new Client[2];
         for(int i = 0; i < c.length; i++)  {
-            c[i] = new Client("http://127.0.0.1:557");
+            c[i] = new Client("http://127.0.0.1:8001");
             c[i].seConnecter();
         }
 
